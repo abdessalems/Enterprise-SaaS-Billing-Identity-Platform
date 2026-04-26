cbd5324fb27452f92efe9412e277aa45f8e5787d
unknown <abdessalemsaa@gmail.com>
2026-04-26 23:59:23 +0200

    Initial commit

---
+++ Enterprise SaaS-Billing-Identity-Platform/src/main/java/com/saadaoui/enterprisesaasbillingidentityplatform/EnterpriseSaaSBillingIdentityPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.saadaoui.enterprisesaasbillingidentityplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseSaaSBillingIdentityPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseSaaSBillingIdentityPlatformApplication.class, args);
+    }
+
+}
---
+++ Enterprise SaaS-Billing-Identity-Platform/src/test/java/com/saadaoui/enterprisesaasbillingidentityplatform/EnterpriseSaaSBillingIdentityPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadaoui.enterprisesaasbillingidentityplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseSaaSBillingIdentityPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
